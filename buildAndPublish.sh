fbd78e72bb8c589166a39d670ae90eeff42acb2c
Jordi Puigdellívol <jordi@gloobus.net>
2019-06-20 21:41:03 +0200

    Builded

---
+++ buildAndPublish.sh
@@ -0,0 +1,5 @@
+bundle exec middleman build --clean
+git commit -am "Builded"
+git push
+curl https://forge.laravel.com/servers/277636/sites/783843/deploy/http?token=vxq7bgODRVM8nIziXGLjkZHVebXAFBnfFEbXegL9
+
